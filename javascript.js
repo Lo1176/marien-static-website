95d81ad8bcd4bf320943dd9e474c063515505538
Laurent Binder <laurentbinder@gmail.com>
2020-12-09 21:21:15 +0100

    add yoga.html

--- javascript.js
+++ javascript.js
@@ -38,6 +38,8 @@ const initLetters = () => {
 }
 initLetters();
 
+var language = window.navigator.userLanguage || window.navigator.language;
+alert(language.substring(0, 2));
 // // Language translation for YOU MARIEN
 // let langdata = {
 //   "languages": {

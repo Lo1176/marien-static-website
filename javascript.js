2b0a3b67112a6de40b4dc6571ab3d899bb53de32
Laurent Binder <laurentbinder@gmail.com>
2020-12-12 16:33:51 +0100

    chnages js nav.html

--- javascript.js
+++ javascript.js
@@ -38,7 +38,7 @@ const initLetters = () => {
 }
 initLetters();
 
-var language = window.navigator.userLanguage || window.navigator.language;
+// var language = window.navigator.userLanguage || window.navigator.language;
 // alert(language.substring(0, 2));
 
 // // Language translation for YOU MARIEN
